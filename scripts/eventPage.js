eccca72a307539cf1a2fd97a3e103ced346303ef
Jeril Albi <devjerilalbi@gmail.com>
2024-02-03 22:39:13 +0530

    image changes with the category

--- scripts/eventPage.js
+++ scripts/eventPage.js
@@ -1,5 +1,7 @@
 
-function changeActiveCategoryCard(category){
-    document.getElementsByClassName("active-category")[0].classList.remove("active-category")
+function changeImageCategory(category){
+    document.getElementsByClassName("active-category")[0].classList.remove("active-category");
     document.getElementById(category).classList.add("active-category");
+    document.getElementsByClassName("active-grid")[0].classList.remove("active-grid");
+    document.getElementById("images_"+category).classList.add("active-grid");
 }
